5ac00351660ab77478f91a2f475824f4b1f6342e
Nur Uddin Ibne Huda <nuruddin.simon@gmail.com>
2024-01-21 01:36:24 +0600

    Remove ignored files

--- APIGateWay/Handler/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('test/', views.hello_view, name='hello'),
-    path('HomeData/', views.HomeData, name='HomeData'),
-    
-    # Add more URL patterns as needed
-]
