988748e6369fbbe9670953560ffbe964a98f1a1a
Rakibul Hasan Rafi <92964753+rafi524@users.noreply.github.com>
2024-01-23 23:41:03 +0600

    Remove ignored files

--- PropertyRegistration/Register/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('ok/', views.ok_view, name='ok'),
-   
-]
--- PropertyRegistration/Register/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-def ok_view(request):
-    result ={
-        'message': 'Hello, ok!',
-    }
-    return JsonResponse(result)
-
