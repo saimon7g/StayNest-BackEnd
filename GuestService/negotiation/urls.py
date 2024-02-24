27653ae714e4fed24a351ad235c41fb181e38193
Nur Uddin Ibne Huda <nuruddin.simon@gmail.com>
2024-02-25 01:15:21 +0600

    negotiation partially working

--- GuestService/negotiation/urls.py
+++ GuestService/negotiation/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('negotiation_details/<int:negotiation_id>/', views.NegotiationDetailsView.as_view(), name='negotiation-details'),
     path('offer_accepted_by_guest/', views.OfferAcceptedByGuestView.as_view(), name='offer-accepted-by-guest'),
     path('offer_rejected_by_guest/', views.OfferRejectedByGuestView.as_view(), name='offer-rejected-by-guest'),
+    path('offer_accepted_by_host/', views.OfferAcceptedByHostView.as_view(), name='offer-accepted-by-host'),
+    path('offer_rejected_by_host/', views.OfferRejectedByHostView.as_view(), name='offer-rejected-by-host'),
+    # const response = await axios.post(`guest/api/nego/start_negotiation_by_guest/`, data);
+    path('start_negotiation_by_guest/', views.StartNegotiationByGuestView.as_view(), name='start-negotiation-by-guest'),
 
 ]
  
